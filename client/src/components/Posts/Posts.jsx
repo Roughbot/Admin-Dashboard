0a0a3bac81f4da1a0285020e6f6f11657b62ec73
Sathyanarayanan <sathyanarayanan1106@gmail.com>
2024-02-15 15:16:09 +0530

    pagination

--- client/src/components/Posts/Posts.jsx
+++ client/src/components/Posts/Posts.jsx
@@ -1,20 +1,26 @@
-import {
-  Box,
-  Card,
-  CardContent,
-  CardMedia,
-  Typography,
-  CardActions,
-  Collapse,
-  Button,
-  useMediaQuery,
-  Divider,
-} from "@mui/material";
+import { Divider } from "@mui/material";
 
 import PostCard from "./PostCard";
 import { useGetDataQuery } from "../../state/getDataApi";
+import { Pagination } from "@mui/material";
+import { useState } from "react";
+
 const Posts = () => {
   const { data, isLoading } = useGetDataQuery();
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 15;
+
+  const indexOfLastItem = page * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  const currentItems = data
+    ? data.slice(indexOfFirstItem, indexOfLastItem)
+    : [];
+
+  const totalPages = data ? Math.ceil(data.length / itemsPerPage) : 0;
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
   return (
     <div className="flex flex-col bg-zinc-200 w-full h-full">
@@ -26,13 +32,22 @@ const Posts = () => {
       </div>
       <Divider />
       <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-3 pt-6 ml-6 mr-6">
-        {data || !isLoading ? (
-          data.map((item) => {
+        {currentItems || !isLoading ? (
+          currentItems.map((item) => {
             return <PostCard key={item._id} props={item} />;
           })
         ) : (
           <p>Loading...</p>
         )}
+      </div>
+      <div className="flex justify-center mt-10">
+        <Pagination
+          shape="rounded"
+          variant="outlined"
+          count={totalPages}
+          page={page}
+          onChange={handleChange}
+        />
       </div>
     </div>
   );
